fa469939786e1a7fe0797f8e3d9a9da0c2e80ec5
Tyler Hughes <hughest8@vcu.edu>
2025-06-04 13:30:46 -0400

    Added more device info and error handling for connection

--- spec_test/connectiontest.py
+++ spec_test/connectiontest.py
@@ -1,25 +1,35 @@
 from Device_Drivers.stellarnet_driverLibs import stellarnet_driver3 as sn
 
-connection = sn.deviceConnectionCheck(0)
-print("Connection status:", connection)
+try:
+    total_device_count_num = sn.total_device_count()
+    print("Total Device Count:", total_device_count_num)
 
+    if total_device_count_num == 0:
+        print("No Devices found.")
+    else:
+        device_index = 0 # can change this to a value
+        connection = sn.deviceConnectionCheck(0)
+        print("Connection status:", connection)
 
-device_info = sn.print_info(0)
-print("Device Info:", device_info)
+        device_info = sn.print_info(0)
+        print("Device Info:", device_info)
 
-device_connection = sn.deviceConnectionCheck(0)
-print("Device Connection Status:", device_connection)
+        device_connection = sn.deviceConnectionCheck(0)
+        print("Device Connection Status:", device_connection)
 
-device_ID = sn.getDeviceId(0)
-print("Device ID:", device_ID)
+        device_ID = sn.getDeviceId(0)
+        print("Device ID:", device_ID)
 
-total_device_count_num = sn.total_device_count()
-print("Total Device Count:", total_device_count_num)
+        total_device_count_num = sn.total_device_count()
+        print("Total Device Count:", total_device_count_num)
 
-device_version = sn.version()
-print("Device Version:", device_version)
+        device_version = sn.version()
+        print("Device Version:", device_version)
 
-device_id = sn.getFullDeviceID(0)
-print("Full Device ID:", device_id)
+        device_id = sn.getFullDeviceID(0)
+        print("Full Device ID:", device_id)
+
+except Exception as e:
+    print("Error communicating with the spec",e)
 
 
